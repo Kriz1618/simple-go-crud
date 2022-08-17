bc8bacf3fec6110ba4ac13f956faed8e0e27096e
Christian <krizmarles@gmail.com>
2022-08-16 22:55:57 -0500

    Added mongodb integration

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 
+## Init go project
+`go mod init github.com/Kriz1618/go-react-crud`
+
+## Install dependecies
+`go get github.com/gofiber/fiber/v2`
+`go get go.mongodb.org/mongo-driver`
+`go get go.mongodb.org/mongo-driver/mongo`
+`go install github.com/cosmtrek/air@latest`
 
 ## Run
-```go run .```
+`go run .` 
+
+*In another console type `cd client/ && npm run start`*
+
+
+## Deploy to heroku
+* `heroku login`
+* `heroku create go-reat-crud`
+* git flow `git add . / git commit -m`
+* `git push heroku master`
+
+**After pushing the code run build in the client, if deploy fails, modify the go version to the previous and try again**
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+
+import './index.css';
 
 const App = () => {
+  const [name, setName] = useState('');
+  const [users, setUsers] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    await fetch(import.meta.env.VITE_API + '/users', {
+      method: 'POST',
+      body: JSON.stringify({ name }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    setName('');
+
+    await loadUsers();
+  }
+
+  async function loadUsers() {
+    const response = await fetch(import.meta.env.VITE_API + "/users");
+    const data = await response.json();
+
+    setUsers(data.users || []);
+  }
+
+  useEffect(() => {
+    loadUsers()
+  }, []);
+
   return (
     <div>
-      <h1>Hello</h1>
-      <button onClick={async () => {
-        const response = await fetch('http://localhost:3000/users');
-        const data = await response.json();
-        console.log('data', data);
-      }}>Get data</button>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='Type your name' value={name} onChange={e => setName(e.target.value)} />
+        <button>Save</button>
+      </form>
 
+      <ul>
+        {users.map(user => (
+          <li key={user._id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   )
 }
